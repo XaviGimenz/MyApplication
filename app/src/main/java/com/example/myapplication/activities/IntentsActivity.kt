a29f99940317074ac0e4cd10eda6adae66a23092
xavier <adelais.caulfield@outlook.com>
2023-03-03 10:07:41 +0100

    Aprendiendo a utilizar intents

--- app/src/main/java/com/example/myapplication/activities/IntentsActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/IntentsActivity.kt
@@ -1,12 +1,41 @@
 package com.example.myapplication.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityIntentsBinding
 
 class IntentsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntentsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intents)
+        binding = ActivityIntentsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonIntentExtras.setOnClickListener { goIntentExtras() }
+        binding.buttonIntentFlags.setOnClickListener { goIntentFlags() }
+        binding.buttonIntentObjects.setOnClickListener { goIntentObject() }
+    }
+
+    private fun goIntentExtras(){
+        val intent = Intent(this, intentExtrasActivity::class.java)
+        intent.putExtra("name", "Xavi")
+        intent.putExtra("lastName", "Gimenez")
+        intent.putExtra("age", 30)
+        intent.putExtra("developer", true)
+        startActivity(intent)
+
+    }
+    private fun goIntentFlags(){
+        val intent = Intent()
+        startActivity(intent)
+
+    }
+    private fun goIntentObject(){
+        val intent = Intent()
+        startActivity(intent)
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/activities/intentExtrasActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityIntentExtrasBinding
+
+class intentExtrasActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntentExtrasBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntentExtrasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getIntentExtrasFromPreviousActivity()
+        binding.buttonBack.setOnClickListener { goBack() }
+    }
+
+    private fun getIntentExtrasFromPreviousActivity(){
+
+        val name: String? = intent.getStringExtra("name")
+        val lastName: String? = intent.getStringExtra("lastName")
+        val age = intent.getIntExtra("age", 0)
+        val developer = intent.getBooleanExtra("developer", false)
+
+        if(name != null && lastName != null && age > 0) {
+            binding.textViewName.text = name
+            binding.textViewLastName.text = lastName
+            binding.textViewAge.text = "$age"
+            binding.checkBoxDeveloper.isChecked = developer
+        }
+    }
+    private fun goBack(){
+        val intent = Intent(this, IntentsActivity::class.java)
+        startActivity(intent)
+    }
+}
